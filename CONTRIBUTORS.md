03e39fcf435fffd5403375027cefdc84e2b944df
unknown <savithlal>
2023-03-15 16:25:50 +0530

    GuruveSaranam

--- CONTRIBUTORS.md
+++
@@ -1,5 +0,0 @@
-### Main contributors so far:
-
-* Jesús Darío - [github](https://github.com/jsdario)
-* Fran Ríos - [github](https://github.com/franrios)
-* Pablo Pizarro - [github](https://github.com/pablopi)
--- index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import WidgetTask from './widgetTask';
-
-AppRegistry.registerComponent('androidWidgetPoc', () => App);
-AppRegistry.registerHeadlessTask('WidgetTask', () => WidgetTask);
-
